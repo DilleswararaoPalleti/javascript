3dc2389056976b8f0412773e62fa652c181d53a8
DilleswararaoPalleti <dilleswararaopalleti@gmail.com>
2026-05-29 21:08:49 +0530

    Scope and lexical environment

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-getname();
-console.log(x);
-var x = 7;
+// getname();
+// console.log(x);
+// var x = 7;
 
-function getname() {
-  console.log("This is my function");
+// function getname() {
+//   console.log("This is my function");
+// }
+
+// console.log(x);
+// console.log(getname);
+//---------scope and functions----
+function a() {
+  var b = 10;
+  c();
+  function c() {
+    console.log(b);
+  }
 }
-
-console.log(x);
-console.log(getname);
+a();
